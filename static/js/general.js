e59d1f4574829bf788ebc722777a9ff312281605
Paz Garcia <pgarcia@russellbedford.mx>
2020-01-23 13:05:03 -0600

    Agregar sidebar y top navbar

---
+++ static/js/general.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var me = this;
+    $("#btnOpenSatLink").click(function(){
+        // window.open("https://www.sat.gob.mx/personas/factura-electronica","_blank","width=400,height=350,top=200,left=200");
+        window.open("https://www.sat.gob.mx/personas/factura-electronica","_blank");
+    });
+});
